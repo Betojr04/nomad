3490d9379857058c261317f6cf855039c61b087f
Betojr04 <betomasia12@gmail.com>
2023-12-29 23:10:05 -0700

    added a logout route that will need to have client side code added to make it work added a route to GET user profile

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -66,3 +66,32 @@ def login_user():
     
     except Exception as e:
         return jsonify({'error': str(e)}), 500
+
+# ROUTE FOR LOGGING OUT A USER
+@api.route('/logout', methods=['POST'])
+@jwt_required()
+def logout_user():
+    return jsonify({'message': "Logout successful"}), 200
+    
+
+# ROUTE FOR GETTING A USER'S PROFILE
+@api.route('/get_profile', methods=['GET'])
+@jwt_required()
+def get_user_profile():
+    try:
+        current_user = get_jwt_identity()
+        user = User.query.filter_by(username=current_user).first()
+        
+        if not user:
+            return jsonify({'error': "User does not exist"}), 404
+    
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+    
+    user_data = user.serialize()
+    return jsonify({'user': user_data}), 200
+    
+
+# ROUTE FOR UPDATING A USER'S PROFILE
+
+# ROUTE FOR DELETING A USER'S PROFILE
