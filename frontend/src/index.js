56f184cf1aa544e7757e92a30f7802f314fd1d6b
Betojr04 <betomasia12@gmail.com>
2023-12-30 22:46:41 -0700

    erased template react assets added bootstrap into html file and install created footer and navbar files, register file created layout file and implemented lazy loading, error boundar , and suspense for faster performance imported layout into index for rendering

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,16 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import { store } from "./store";
-import "./index.css";
-import App from "./App";
+import { store } from "./store/store.js";
+import { Layout } from "./views/Layout.js";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <Layout />
     </Provider>
   </React.StrictMode>
 );
